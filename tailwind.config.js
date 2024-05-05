d063be847344537bc90a1988fe84189ed2aa32a7
AbdullahHamadax <abdallahamada114@gmail.com>
2024-05-05 12:27:21 +0300

    Removed js from taildwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/*.{html}"],
   theme: {
     extend: {
       fontFamily: {
